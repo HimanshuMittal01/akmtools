fd03465a793c5bf3e70650f5c1cd20bcd004f0b4
Himanshu Mittal <himanshu.mittal.dev@gmail.com>
2024-10-21 12:21:12 +0530

    feat: content about pages

--- streamlitapp/pages/about.py
+++ streamlitapp/pages/about.py
@@ -4,4 +4,7 @@ Home Page
 
 import streamlit as st
 
-st.write("Hello World to AKM Tools")
+st.title("Welcome to AKM Tools 🛠️")
+st.write("AKM Tools makes financial data work simple, fast, and elegant.")
+st.write("Developed by [Himanshu Mittal](https://www.linkedin.com/in/himanshumittal13/), powered by [Ashok Mittal & Associates](https://ashokmittal.com).")
+st.success("Got feedback or suggestions? Drop us an email at [contact@ashokmittal.com](mailto:contact@ashokmittal.com).")
--- streamlitapp/pages/gst_interest.py
+++ streamlitapp/pages/gst_interest.py
@@ -4,8 +4,13 @@ GST Interest Calculation Page
 
 import polars as pl
 import streamlit as st
+import fastexcel
 
 from akmtools.gst_interest import calculate_gst_180days_interest
+
+st.title("Interest on Late Payment of GST")
+st.write("According to the GST Penalty regulations, interest will be charged at the rate of 18 percent per annum from the taxpayers who fail to pay their taxes on time. The interest will be levied for the days after the due date.")
+st.divider()
 
 uploaded_file = st.file_uploader("Choose a file")
 
@@ -18,18 +23,42 @@ if uploaded_file is not None:
         df = pl.read_csv(uploaded_file)
     else:
         try:
-            df = pl.read_excel(uploaded_file)
+            # Read sheet names
+            sheet_names = fastexcel.read_excel(uploaded_file.getvalue()).sheet_names
+
+            # Select sheet
+            sheet_name = st.selectbox(
+                "Select sheet:",
+                sheet_names
+            )
+
+            # Read excel
+            df = pl.read_excel(uploaded_file, sheet_name=sheet_name)
         except Exception as e:
             st.warning("Only csv/xlsx format are supported.")
 
     if df is not None:
         output = calculate_gst_180days_interest(df)
 
-        st.download_button(
-            label='Download Interest Sheet',
-            data=output,
-            file_name=f'{uploaded_filename}-gst_interest_akm.xlsx'
-        )
+        col1, col2, col3 = st.columns(3)
+        with col2:
+            st.download_button(
+                label='Download Interest Sheet',
+                data=output,
+                file_name=f'{uploaded_filename}-gst_interest_akm.xlsx',
+                type='primary'
+            )
 
 else:
-    st.warning("you need to upload a csv or excel file.")
+    st.warning("You need to upload a csv or excel file.")
+
+# Write rules
+st.info(
+"""
+Three columns are expected - 'Date', 'Debit', 'Credit'. Optionally, provide 'GST\%' column otherwise 18\% is used as default for all bills.
+
+Keep the following criteria in mind:
+- Values in 'Date' column are assumed to be sorted.
+- Columns 'Debit' and 'Credit' should have positive values.
+"""
+)
